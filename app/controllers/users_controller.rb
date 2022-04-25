2ab5c1d0577b5d84e35c13d912eaa10a28715bc0
kristophertope <kristophertope@gmail.com>
2022-04-25 14:47:48 -0500

    add error styling and testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,15 +11,16 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # TO-DO
+      # Handle a successful save.
     else
       render 'new'
     end
+  end
 
-    private
-      def user_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation)
-      end
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
